acd6535f96b4e8aec74274a2ea418404430b92b0
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-12 23:07:58 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import {Text, View} from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <Text>Teste</Text>
+      <Text>Hello. My name is Trung</Text>
     </View>
   );
-}
+};
 
 export default App;

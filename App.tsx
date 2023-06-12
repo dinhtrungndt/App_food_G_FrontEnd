de5d460f0bf719f93a9c602f0da67ad3bccaaa46
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-12 23:05:51 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+export default App;

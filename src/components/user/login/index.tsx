ea67bbfb9cac9f8808f889a641bcf6f47a9b1416
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-13 14:24:25 +0700

    Login Screens

---
+++ src/components/user/login/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginScreens = () => {
+  return (
+    <View>
+      <Text>LoginScreens</Text>
+    </View>
+  )
+}
+
+export default LoginScreens
+
+const styles = StyleSheet.create({})

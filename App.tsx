bc9d1b611ae923fe9aeeb7660917e992a7bb07b0
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-14 00:57:26 +0700

    Up Cart

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import CartScreens from './src/components/listFood/Cart';
 
 const App = () => {
   return (
     <View>
-      <Text>Hello. My name is Trung</Text>
+      <CartScreens/>
     </View>
   );
 };
---
+++ src/components/listFood/Cart/Data/index.tsx
@@ -0,0 +1,34 @@
+export const Data = [
+    {
+        id: '1',
+        name: 'Red Apple',
+        price: `$`+2.495,
+        image: require('../../../../../image/card_apple_list.png'),
+        quantity: 1,
+        gram: 'kg',
+    },
+    {
+        id: '2',
+        name: 'Orginal Banana',
+        price: `$`+2.995,
+        image: require('../../../../../image/card_banana_list.png'),
+        quantity: 1,
+        gram: 'kg',
+    },
+    {
+        id: '3',
+        name: 'Avocado Bowl',
+        price: `$`+24,
+        image: require('../../../../../image/card_bowl_list.png'),
+        quantity: 1,
+        gram: 'st',
+    },
+    {
+        id: '4',
+        name: 'Salmon',
+        price: `$`+25,
+        image: require('../../../../../image/card_salmon_list.png'),
+        quantity: 1,
+        gram: 'kg',
+    },
+]

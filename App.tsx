f6b7314f648ffd882a05d1dd16ad7d4dc0e17345
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-24 14:13:49 +0700

    Revert "Up Boarding"
    This reverts commit dbe8483f307437bd59f19e94f75564217b7782ec.

--- App.tsx
+++ App.tsx
@@ -6,14 +6,13 @@ import ItemFavoriteScreens from './src/components/listFood/Favorite/Item';
 import {Text, View, SafeAreaView} from 'react-native';
 import LoginScreens from './src/components/user/login';
 import HomeScreens from './src/components/listFood/Home';
-import BoardingScreens from './src/screens/Boarding/Boarding';
 
 const App = (): React.JSX.Element => {
   return (
-    <View>
+    <SafeAreaView>
       {/* <LoginScreens/> */}
-      <BoardingScreens />
-    </View>
+      <LoginScreens />
+    </SafeAreaView>
   );
 };
 

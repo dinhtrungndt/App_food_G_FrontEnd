87b0b13d3ab072d5c3388b15bed50d4518e611bb
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-24 14:03:45 +0700

    Revert "Merge branch 'Chinh'"
    This reverts commit 3cb798716b131730de72e0392747e8bab9c546d4, reversing
    changes made to 486839f7524ec75aef8cd5151e505d69b6e60295.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
+import {Text, View} from 'react-native';
 import AccountScreens from './src/components/listFood/Account';
 import CartScreens from './src/components/listFood/Cart';
 import FavoriteScreens from './src/components/listFood/Favorite';
 import ItemFavoriteScreens from './src/components/listFood/Favorite/Item';
-import {Text, View, SafeAreaView} from 'react-native';
-import LoginScreens from './src/components/user/login';
-import HomeScreens from './src/components/listFood/Home';
 
-const App = (): React.JSX.Element => {
+const App = () => {
   return (
-    <SafeAreaView>
-      {/* <LoginScreens/> */}
-      <HomeScreens />
-    </SafeAreaView>
+    <View>
+      <AccountScreens />
+    </View>
   );
 };
 

9abfff61d2a4dcec945cd8281cfac00b9cdc5f52
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-24 14:12:34 +0700

    Revert "Up FrontEnd"
    This reverts commit bde7af1aabe8f8d53872bc55c60f9ae9a59ba591.

--- App.tsx
+++ App.tsx
@@ -10,12 +10,11 @@ import BoardingScreens from './src/screens/Boarding/Boarding';
 import SignUpScreens from './src/components/user/signup/name';
 import SignUpPassScreens from './src/components/user/signup/pass';
 import CodePhoneScreens from './src/components/user/signup/codePhone';
-import ExploreScreens from './src/components/listFood/Explore';
 
 const App = (): React.JSX.Element => {
   return (
     <View>
-      <HomeScreens />
+      <CodePhoneScreens />
     </View>
   );
 };

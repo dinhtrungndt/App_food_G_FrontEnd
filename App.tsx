90283bbbb0f37223e35b5fe0ab1b705c8bd59cb8
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-24 14:02:45 +0700

    Revert "Up library and Stack"
    This reverts commit 2289b9d6e39469d78795bf3fa719833b009e8279.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import AccountScreens from './src/components/listFood/Account';
 import CartScreens from './src/components/listFood/Cart';
@@ -13,31 +12,11 @@ import SignUpPassScreens from './src/components/user/signup/pass';
 import CodePhoneScreens from './src/components/user/signup/codePhone';
 import ExploreScreens from './src/components/listFood/Explore';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {
-  RootStackParamList,
-  RootStackScreensEnum,
-} from './src/components/listFood/RootStackParamList';
-
-const Stack = createStackNavigator<RootStackParamList>();
-
 const App = (): React.JSX.Element => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name={RootStackScreensEnum.Home}
-          component={HomeScreens}
-          options={{}}
-        />
-        <Stack.Screen
-          name={RootStackScreensEnum.Explore}
-          component={ExploreScreens}
-          options={{}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <HomeScreens />
+    </View>
   );
 };
 

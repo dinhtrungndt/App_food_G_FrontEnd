a91aa4bd6b30551f9068db053321e2ae621cbe50
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-06-27 09:00:28 +0700

    Up load Boarding

--- App.tsx
+++ App.tsx
@@ -7,12 +7,12 @@ import {Text, View, SafeAreaView} from 'react-native';
 import LoginScreens from './src/components/user/login';
 import HomeScreens from './src/components/listFood/Home';
 import ExploreScreens from './src/components/listFood/Explore';
+import BoardingScreens from './src/screens/Boarding/Boarding';
 
 const App = (): React.JSX.Element => {
   return (
     <SafeAreaView>
-      {/* <LoginScreens/> */}
-      <ExploreScreens />
+      <BoardingScreens />
     </SafeAreaView>
   );
 };
